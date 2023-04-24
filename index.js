0adb0fca8dc571c244590ca808e61c182de7eca3
Pana Wanit <pnaw.wan@gmail.com>
2023-04-24 19:35:25 +0700

    Add scroll animation onload subject

--- index.js
+++ index.js
@@ -8,11 +8,15 @@ import {assignments, signedCourse} from './demo.js';
 drawAllSignedCourse(signedCourse);
 drawAllAssignment(assignments);
 
+var lastDrawAssignment = false; // to fix re-reload all course animation
 
 document.getElementById("courses").addEventListener("click", function(event) {
+    if(lastDrawAssignment)
+        return;
     drawAllAssignment(assignments);
     for(let i=0; i<document.querySelectorAll(".course").length; i++)
         document.querySelectorAll(".course")[i].classList.remove("course-selected");
+    lastDrawAssignment = true;
 });
 
 for(let i=0; i<document.querySelectorAll(".course").length; i++) {
@@ -22,13 +26,17 @@ for(let i=0; i<document.querySelectorAll(".course").length; i++) {
         
         if(course.classList.contains("course-selected")) {
             course.classList.remove("course-selected");
-            drawAllAssignment(assignments);
+            if(lastDrawAssignment)
+                return;
+            drawAllAssignment(assignments),
+            lastDrawAssignment = true;
         }
         else {
             for(let i=0; i<document.querySelectorAll(".course").length; i++)
                 document.querySelectorAll(".course")[i].classList.remove("course-selected");
             course.classList.add("course-selected");
             drawCourseAssignment(assignments, id);
+            lastDrawAssignment = false;
         }
     });
 }
